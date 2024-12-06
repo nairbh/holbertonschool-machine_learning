51bf201d7453b86b313508367c199c579717a294
nairbh <houcinenair@proton.me>
2024-12-06 12:07:46 +0100

    update code for return a new matrix not a old one

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -5,4 +5,4 @@
 def matrix_transpose(matrix):
     """ function that returns the transpose of a 2D matrix """
     a = [[row[i] for row in matrix] for i in range(len(matrix[0]))]
-    return matrix
+    return a
