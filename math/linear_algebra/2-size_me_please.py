21acb92758fe08637a52fb879bc4149b7837ff4f
nairbh <houcinenair@proton.me>
2024-12-06 11:11:12 +0100

    fix pycodes style

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -2,4 +2,5 @@
 
 def matrix_shape(matrix):
     """ find the shape of Matrix """
-    return [len(matrix)] + matrix_shape(matrix[0]) if isinstance(matrix, list) and matrix else []
+    return [len(matrix)] + matrix_shape(matrix[0]
+                                        ) if isinstance(matrix, list) and matrix else []
