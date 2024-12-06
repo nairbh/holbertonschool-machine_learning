a3d4650afd09c33aa278375e3712cc10d0ebf542
nairbh <houcinenair@proton.me>
2024-12-06 14:51:35 +0100

    adds two arrays element-wise

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""   adds two arrays element-wise """
+
+
+def add_arrays(arr1, arr2):
+    """  adds two arrays element-wise """
+
+    if (len(arr1) != len(arr2)):
+        return None
+    return [arr1[i] + arr2[i] for i in range(len(arr1))]
